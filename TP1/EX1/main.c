4f4bc9e567ddc3f80c1e96f54e8885f34e3731ea
SakaiMarouane <80362738+SakaiMarouane@users.noreply.github.com>
2021-06-30 20:03:11 +0100

    Create main.c

---
+++ TP1/EX1/main.c
@@ -0,0 +1,19 @@
+include "Fibo.h"
+include "Fibo.c"
+
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  // int result = recfib(n);
+  // printf("%d\n" , result);
+  // int result2 = fibo(n);
+  // printf("%d\n" , result2);
+  // int result3 = isprime(n);
+  // printf("%d\n" , result3);
+  // getprime(n);
+  int result5 = isprime(n);
+  printf("%d\n" , result5);
+  return 0;
+}
