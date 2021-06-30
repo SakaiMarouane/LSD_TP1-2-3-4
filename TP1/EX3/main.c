c612dc2a809c80ac658fda6266c7667b0ac4b06d
SakaiMarouane <80362738+SakaiMarouane@users.noreply.github.com>
2021-06-30 20:08:49 +0100

    Create main.c

---
+++ TP1/EX3/main.c
@@ -0,0 +1,14 @@
+#include "programme.h"
+#include "programme.c"
+
+int main()
+{
+int n;
+scanf("%d", &n);
+int result3 = isprime(n);
+printf("%d\n" , result3);
+getprime(n);
+int result5 = isprime(n);
+printf("%d\n" , result5);
+return 0;
+}
